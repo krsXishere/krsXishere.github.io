a2c4f6210f2354e7527641dfc0ddfd3a1c3b13ca
KrsXishere <lxghozt28@gmail.com>
2024-02-27 23:24:49 +0700

    Inital commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
- const mntoggle = document.querySelector('.menu-toggle input');
- const nav = document.querySelector('nav ul');
+//  const mntoggle = document.querySelector('.menu-toggle input');
+//  const nav = document.querySelector('nav ul');
 
-mntoggle.addEventListener('click',function(){
-    nav.classList.toggle('menushow');
-})
+// mntoggle.addEventListener('click',function(){
+//     nav.classList.toggle('menushow');
+// })
+
+// main.js
+document.getElementById('nav-toggle').addEventListener('change', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show', this.checked);
+});
